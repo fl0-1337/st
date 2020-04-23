a415bbd10da08296a2a0957579b85c96c12a3fea
Flo <flovogl@protonmail.com>
2020-04-23 12:24:10 +0200

    switch to nord theme

--- config.h
+++ config.h
@@ -83,47 +83,40 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-  /* 8 normal colors */
-  [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-  [1] = "#cc241d", /* red     */
-  [2] = "#98971a", /* green   */
-  [3] = "#d79921", /* yellow  */
-  [4] = "#458588", /* blue    */
-  [5] = "#b16286", /* magenta */
-  [6] = "#689d6a", /* cyan    */
-  [7] = "#a89984", /* white   */
 
-  /* 8 bright colors */
-  [8]  = "#928374", /* black   */
-  [9]  = "#fb4934", /* red     */
-  [10] = "#b8bb26", /* green   */
-  [11] = "#fabd2f", /* yellow  */
-  [12] = "#83a598", /* blue    */
-  [13] = "#d3869b", /* magenta */
-  [14] = "#8ec07c", /* cyan    */
-  [15] = "#ebdbb2", /* white   */
 
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+const char *colorname[] = {
+  "#3b4252", /*  0: nord1 */
+  "#bf616a", /*  1: nord11 */
+  "#a3be8c", /*  2: nord14 */
+  "#ebcb8b", /*  3: nord13 */
+  "#81a1c1", /*  4: nord9 */
+  "#b48ead", /*  5: nord15 */
+  "#88c0d0", /*  6: nord8 */
+  "#e5e9f0", /*  7: nord5 */
+  "#4c566a", /*  8: nord3 */
+  "#bf616a", /*  9: nord11 */
+  "#a3be8c", /* 10: nord14 */
+  "#ebcb8b", /* 11: nord3 */
+  "#81a1c1", /* 12: nord9 */
+  "#b48ead", /* 13: nord15 */
+  "#8fbcbb", /* 14: nord7 */
+  "#eceff4", /* 15: nord6 */
+  [255] = 0,
+  // defaults
+  [256] = "#d8dee9", /* 256: nord4 */
+  [257] = "#2e3440", /* 257: nord0 */
 };
 
-
 /*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-/* unsigned int defaultfg = 7; */
-/* unsigned int defaultbg = 0; */
-/* static unsigned int defaultcs = 7; */
-
-unsigned int defaultfg = 15;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 15;
-static unsigned int defaultrcs = 257;
+* Default colors (colorname index)
+* foreground, background, cursor, reverse
+* cursor
+*/
+unsigned int defaultfg = 256; /* nord4 */
+unsigned int defaultbg = 257; /* nord0 */
+unsigned int defaultcs = 256; /* nord4 */
+unsigned int defaultrcs = 8;  /* nord3 */
 
 /*
  * Default shape of cursor
